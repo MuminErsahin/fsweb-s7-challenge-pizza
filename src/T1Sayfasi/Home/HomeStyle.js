fd3ed751ea87c6c3ac3730c3b400ee6696a64ead
MüminErsahin <75380505+MuminErsahin@users.noreply.github.com>
2024-07-20 13:55:53 +0300

    home-banneer

--- src/T1Sayfasi/Home/HomeStyle.js
+++ src/T1Sayfasi/Home/HomeStyle.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 
 
 export const Container = styled.div`
-  background: url("./Assets/mile2-aseets/home-bg.png");
+  background: url("./Assets/mile1-aseets/home-banner.png");
   width: 100%;
   height: 100vh;
   background-size: cover;
